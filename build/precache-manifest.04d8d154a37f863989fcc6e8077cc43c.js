f4299a51e49992597ede3b276944f64f66b183a4
vaishnavi patil <vp397264@gmail.com>
2022-08-02 11:05:53 +0530

    build added

---
+++ build/precache-manifest.04d8d154a37f863989fcc6e8077cc43c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea474839ccc691bdc7ec27e8782ae2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75d30bc0fb1a5f8b233d",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "94722200f2bb2c54f87b",
+    "url": "/static/js/2.32d91e32.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.32d91e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d30bc0fb1a5f8b233d",
+    "url": "/static/js/main.7edd3d23.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
